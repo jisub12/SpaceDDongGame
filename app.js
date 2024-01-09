cd3f8e3bd8c988f53b449693c562993e8a945ef9
jisub12 <jisub11@nate.com>
2024-01-09 17:47:06 +0900

    01091547 서버 추가

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors({
+    origin: "http://127.0.0.1:5000/",
+    credentials : true
+}));
+
+app.use(express.static(__dirname));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/main', express.static(path.join(__dirname, 'main')));
+
+app.listen(7777, () => {
+    console.log('7777 Server Open');
+});
